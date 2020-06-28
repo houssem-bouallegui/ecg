baf6f9c67099e5d4b5d3c1ac2acf7fb2750ab014
houssem-bouallegui <32398198+houssem-bouallegui@users.noreply.github.com>
2020-06-28 18:56:33 +0100

    overall fix and changes

--- loginuser.php
+++ loginuser.php
@@ -25,7 +25,7 @@ $dbname = "id8999302_ecgcloud"; /* Database name */
 		session_start();
         $_SESSION['username'] = $myusername;
 		//echo '<script>alert("Session=$username");</script>';
-		header("location: showdatagraph.php");
+		header("location: menu.html");
 	  }
 	  else
 		echo '<script>alert("Invalid Credentials");</script>';
@@ -36,24 +36,21 @@ $dbname = "id8999302_ecgcloud"; /* Database name */
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="w3.css">
+	<link rel="stylesheet" href="login.css">
 	<title>Login Page</title>
 	
 
 </head>
 <body background="a.jpg">
-<body>
-
-<div class="w3-card-4  w3-display-middle w3-sand" style="width:40%" >
-
-<div class="w3-container w3-green"><h2>User Login</h2></div>
-<form method="post" action="" class="w3-container ">
-	 <label style="font-size:16px">USERNAME</label> <input type="text" class="w3-round-xlarge w3-input" name="username" id="Admin" placeholder="Enter Username">
-	 <label style="font-size:16px">PASSWORD</label> <input type="password" class="w3-round-xlarge w3-input" name="password" id="user_password" placeholder="Enter Password">
-	
-	<br>
-	<input class="w3-button w3-green" type="submit">
-	<p> </p>
+<div class="login-page">
+  <div class="form">
+    </form>
+    <form class="login-form">
+      <label style="font-size:14px">Username</label> <input type="text" name="username" id="Admin" placeholder="Enter Username">
+	 <label style="font-size:14px">Password</label> <input type="password" name="password" id="user_password" placeholder="Enter Password">
+      <input type="submit">
+    </form>
+  </div>
 </div>
-</form>
 </body>
 </html>
