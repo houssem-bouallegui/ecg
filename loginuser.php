495b8952b2fae416b577d9019fefc35a8414becd
houssem-bouallegui <32398198+houssem-bouallegui@users.noreply.github.com>
2020-07-01 23:02:07 +0100

    fixes

--- loginuser.php
+++ loginuser.php
@@ -10,14 +10,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       $sql = "SELECT id FROM login WHERE User = '$myusername' and Pass = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+
       
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         session_register("myusername");
+        
          $_SESSION['login_user'] = $myusername;
          
          header("location: menu.html");
